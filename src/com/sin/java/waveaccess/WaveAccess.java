6577907644105b1d944baab028c11d79b1219791
RobinTang <sintrb@gmail.com>
2014-04-23 16:44:05 +0800

    添加Waveaccess说明

--- src/com/sin/java/waveaccess/WaveAccess.java
+++ src/com/sin/java/waveaccess/WaveAccess.java
@@ -1,11 +1,12 @@
 package com.sin.java.waveaccess;
 
-
 /**
+ * 波形文件(.wav)的读写操作，目前只支持16bit的PCM编码方式
+ * 
  * @author Robin
  * @version 1.0
  * @see https://github.com/sintrb/WaveAccess
  */
 public class WaveAccess {
-	public static final String VERSION =  "1.0";
+	public static final String VERSION = "1.0";
 }
