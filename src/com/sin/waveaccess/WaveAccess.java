5b66a6105cdd4b814329f222e53b4c8c5cbd5920
RobinTang <sintrb@gmail.com>
2014-04-23 16:12:51 +0800

    init

---
+++ src/com/sin/waveaccess/WaveAccess.java
@@ -0,0 +1,11 @@
+package com.sin.waveaccess;
+
+
+/**
+ * @author Robin
+ * @version 1.0
+ * @see https://github.com/sintrb/WaveAccess
+ */
+public class WaveAccess {
+	public static final String VERSION =  "1.0";
+}
