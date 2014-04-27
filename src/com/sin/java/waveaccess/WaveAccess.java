c35312ef7bfd6e93e4914d6ff0ac8f2eeac4b461
RobinTang <sintrb@gmail.com>
2014-04-27 12:36:00 +0800

    V1.1完成，支持16bit和8bitPCM编码的单双声道文件操作

--- src/com/sin/java/waveaccess/WaveAccess.java
+++ src/com/sin/java/waveaccess/WaveAccess.java
@@ -1,12 +1,15 @@
 package com.sin.java.waveaccess;
 
 /**
- * 波形文件(.wav)的读写操作，目前只支持16bit的PCM编码方式
+ * 波形文件(.wav)的读写操作，支持16bit和8bitPCM编码的单双声道文件操作
  * 
  * @author Robin
- * @version 1.0
+ * @version 1.1
  * @see https://github.com/sintrb/WaveAccess
+ * 
+ * @Update 1. v1.0 2014-04-24 <br/>
+ *         2. v1.1 2014-04-27
  */
 public class WaveAccess {
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 }
